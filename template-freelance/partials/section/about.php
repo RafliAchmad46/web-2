f97266377ad8d619c6516d60ae3775ee0663ab30
RafliAchmad46 <zulfikarrafli604@gmail.com>
2025-03-08 13:45:49 +0700

    Praktikum 03

---
+++ template-freelance/partials/section/about.php
@@ -0,0 +1,26 @@
+<section class="page-section bg-primary text-white mb-0" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-4 ms-auto"><p style="font-family: 'Arial', sans-serif; font-size: 20px;">My name is Rafli Achmad Zulfikar, and I am currently a student at Nurul Fikri College of Technology. I have a strong passion for technology and innovation, which drives me to excel in my studies.</p></div>
+                    <div class="col-lg-4 me-auto"><p style="font-family: 'Arial', sans-serif; font-size: 20px;">I have proficiency in programming languages such as CSS, Java, JavaScript, PHP, and more. I can collaborate effectively and possess creative ideas</p></div>
+                </div>
+                <!-- About Section Button-->
+                <!-- <div class="text-center mt-4">
+                    <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/theme/freelancer/">
+                        <i class="fas fa-download me-2"></i>
+                        Free Download!
+                    </a>
+                </div> -->
+            </div>
+        </section>
---
+++ template-personal(task)/sections/about.php
@@ -0,0 +1,16 @@
+<section class="bg-light py-5">
+    <div class="container px-5">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-xxl-8">
+                <div class="text-center my-5">
+                    <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
+                    <p class="lead fw-light mb-4">My name is Rafli Achmad Zulfikar and I help brands grow.</p>
+                    <p class="text-muted">I am a engineer with a passion for building beautiful and functional websites. I am a student at STT Terpadu and have experience with a wide range of technologies. I am always looking for new challenges and opportunities to learn and grow.</p>
+                    <a class="text-gradient m-2 width:5px" href="https://www.instagram.com/flichkar/" style="font-size: 1.5rem;"><i class="bi bi-instagram"></i></a>
+                    <a class="text-gradient m-2" href="https://www.linkedin.com/in/rafli-achmad-zulfikar-91b5a030b" style="font-size: 1.5rem;"><i class="bi bi-linkedin"></i></a>
+                    <a class="text-gradient m-2" href="https://github.com/RafliAchmad46" style="font-size: 1.5rem;"><i class="bi bi-github"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
